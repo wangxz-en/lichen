045d1852fe3e1d7b4c07402ff1f16dfae3c958c4
Lucian Branescu Mihaila <lucian.branescu@gmail.com>
2012-04-25 18:12:18 +0100

    Add txredis example + setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lichen',
+    version='0.1',
+    description="Tick-based online AI game",
+    url='http://github.com/lucian1900/lichen',
+
+    packages=find_packages(),
+    install_requires=[
+        'Twisted',
+        'txRedis',
+    ])
